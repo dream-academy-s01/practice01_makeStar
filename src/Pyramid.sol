ee7a7aeb620cbeb96f1f8e67431a89517383b92c
Kwon HyeonKyeong <hktomato@Kwons-MacBook-Pro.local>
2022-09-07 20:24:13 +0900

    make test fail

--- src/Pyramid.sol
+++ src/Pyramid.sol
@@ -1,19 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.13;
 
-
-import "forge-std/console.sol";
-
-
 contract Pyramid {
    function run() view public returns (string memory _out) {
        _out = "";
-       for (uint i = 0; i < 5; i++) {
-           for (uint j = 0; j <= i; j++) {
-               _out = string.concat(_out, "*");
-           }
-           _out = string.concat(_out, "\n");
-       }
-       console.log("Result:\n%s", _out);
+       // todo
    }
 }
